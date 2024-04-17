c31216440b89f44834feba54976d9ca9d4f66956
Mariano Vargas <marianoluisvargas@gmail.com>
2024-04-17 11:43:10 -0300

    feat(ejercicio11): estilos terminados

--- ejercicio11_rollDice.php
+++ ejercicio11_rollDice.php
@@ -12,16 +12,18 @@
         $quantity = intval($_POST['quantity']);
         $total_score = 0; // Inicializar el puntaje total
         echo '<h2>Dados lanzados:</h2>';
-        echo '<div class="dice-container">';
+        echo '<div class="ej11_dice-container">';
         // Lanzar cada dado y mostrar su imagen
         for ($i = 0; $i < $quantity; $i++) {
             $random_value = rand(1, 6); // Generar un valor aleatorio entre 1 y 6
             $total_score += $random_value; // Sumar el valor al puntaje total
-            echo '<img class="dice-image" src="dice/die-' . $random_value . '.png" alt="Dado ' . $random_value . '">';
+            echo '<div class="ej11_bg">';
+            echo '<img class="ej11_dice-image" src="dice/die-' . $random_value . '.png" alt="Dado ' . $random_value . '">';
+            echo '</div>';
         }
         echo '</div>';
         echo '<h2>Puntaje obtenido:</h2>';
-        echo '<p>' . $total_score . '</p>'; // Mostrar el puntaje total
+        echo '<p class="ej11_score">' . $total_score . '</p>'; // Mostrar el puntaje total
     } else {
         echo '<p>No se ha especificado la cantidad de dados.</p>';
     }
